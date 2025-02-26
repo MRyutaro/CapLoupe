9950f9cdfe920b2ac8608e4d60f10835f881e21d
MRyutaro <m2002r1028@icloud.com>
2025-02-26 19:41:50 +0900

    update: ショートカットキーの設定

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NHotkey;
+using NHotkey.Wpf;
+
+namespace src;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        // ショートカットキー（Ctrl + Alt + Z）を登録
+        HotkeyManager.Current.AddOrReplace("Zoom", Key.Z, ModifierKeys.Control | ModifierKeys.Alt, OnZoomShortcut);
+    }
+
+    private void OnZoomShortcut(object sender, HotkeyEventArgs e)
+    {
+        MessageBox.Show("ショートカットキーが押されました！", "通知", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
